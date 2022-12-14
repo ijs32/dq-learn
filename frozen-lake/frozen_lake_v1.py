9bdb2540a0db68837b2f6ba2d93b94fb74bd244e
ijs32 <iansilber32@gmail.com>
2022-12-13 21:02:47 -0500

    frozen lake v1

---
+++ frozen-lake/frozen_lake_v1.py
@@ -0,0 +1,28 @@
+import gym
+import numpy as np
+import matplotlib.pyplot as plt
+
+env = gym.make('FrozenLake-v1', desc=None, map_name="4x4", is_slippery=True)
+
+# LEFT = 0 DOWN = 1 RIGHT = 2 UP = 3
+# By setting the policy below we greatly increase our win chances
+policy = {0: 1, 1: 2, 2: 1, 3: 0, 4: 1, 6: 1, 8: 2, 9: 1, 10: 1, 13: 2, 14: 2}
+n_games = 1000
+win_pct = []
+scores = []
+
+for i in range(n_games):
+    done = False
+    obs, throwaway = env.reset()
+    score = 0
+    while not done:
+        action = policy[obs]
+        obs, reward, done, info, more = env.step(action)
+        score += reward
+    scores.append(score)
+    
+    if i % 10 == 0:
+        average= np.mean(scores[-10:])
+        win_pct.append(average)
+plt.plot(win_pct)
+plt.show()
